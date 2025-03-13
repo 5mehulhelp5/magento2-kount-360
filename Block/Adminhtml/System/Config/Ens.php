aebe94b7a0bff0876515b60679ba87f7bac80a59
Jon Colas <jon.colas@swarmingtech.com>
2025-03-13 16:39:05 -0500

    KOUNT-201 Add update for additional urls if they exist for ens copy in admin panel

--- Block/Adminhtml/System/Config/Ens.php
+++ Block/Adminhtml/System/Config/Ens.php
@@ -3,19 +3,26 @@
  * Copyright (c) 2024 KOUNT, INC.
  * See COPYING.txt for license details.
  */
+
 namespace Kount\Kount360\Block\Adminhtml\System\Config;
 
 use Magento\Config\Block\System\Config\Form\Field;
 use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\UrlInterface;
 
-/**
- * Ens url for backend configuration
- */
 class Ens extends Field
 {
+    /**
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param StoreManagerInterface $storeManager
+     * @param UrlInterface $frontendUrlBuilder
+     * @param array $data
+     */
     public function __construct(
         protected \Magento\Backend\Block\Template\Context $context,
-        protected \Magento\Framework\Url $frontendUrlBuilder,
+        protected StoreManagerInterface $storeManager,
+        protected UrlInterface $frontendUrlBuilder,
         array $data = []
     ) {
         parent::__construct($context, $data);
@@ -29,7 +36,41 @@ class Ens extends Field
      */
     protected function _getElementHtml(AbstractElement $element): string
     {
-        return '<span id="' . $element->getHtmlId() . '">' . $this->getEnsUrl() . '</span>';
+        $html = '<div id="' . $element->getHtmlId() . '">';
+        $stores = $this->_storeManager->getStores();
+
+        if (empty($stores)) {
+            return '<span>' . $this->getEnsUrl() . '</span>';
+        }
+
+        $html .= '<ul style="list-style-type: none; padding-left: 0;">';
+        foreach ($stores as $store) {
+            $storeId = $store->getId();
+            $storeName = $store->getName();
+            $storeCode = $store->getCode();
+            $ensUrl = $this->getEnsUrlForStore($store);
+
+            $html .= '<li style="margin-bottom: 10px;">';
+            $html .= '<strong>' . $storeName . ' (' . $storeCode . '):</strong><br/>';
+            $html .= '<span>' . $ensUrl . '</span>';
+            $html .= '</li>';
+        }
+        $html .= '</ul>';
+        $html .= '</div>';
+
+        return $html;
+    }
+
+    /**
+     * Get ENS URL for a specific store
+     *
+     * @param \Magento\Store\Model\Store $store
+     * @return string
+     */
+    protected function getEnsUrlForStore(\Magento\Store\Model\Store $store): string
+    {
+        $url = $store->getBaseUrl() . 'kount360/ens/';
+        return $url;
     }
 
     /**
@@ -42,15 +83,21 @@ class Ens extends Field
             $store = $this->_storeManager->getWebsite($websiteId)->getDefaultStore();
             $this->frontendUrlBuilder->setScope($store);
         }
-        return $this->frontendUrlBuilder->getUrl('kount360/ens', ['_forced_secure' => true, '_nosid' => true]);
-    }
 
-    /**
-     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
-     * @return string
-     */
-    protected function _renderInheritCheckbox(\Magento\Framework\Data\Form\Element\AbstractElement $element): string
-    {
-        return '<td/>';
+        $url = $this->frontendUrlBuilder->getUrl('kount360/ens', ['_forced_secure' => true, '_nosid' => true]);
+
+        // Ensure the URL includes the /html prefix if it's not already there
+        if (strpos($url, '/html/') === false) {
+            $parsedUrl = parse_url($url);
+            $path = $parsedUrl['path'] ?? '';
+
+            // If path doesn't start with /html, add it
+            if (strpos($path, '/html') !== 0) {
+                $newPath = '/html' . $path;
+                $url = str_replace($path, $newPath, $url);
+            }
+        }
+
+        return $url;
     }
 }
